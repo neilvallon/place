7f2d580a8cbb1c24597d6eb635800d8b5d179ae0
Neil Vallon <neilvallon@gmail.com>
2017-04-01 20:29:46 -0500

    cleanup

--- place.go
+++ place.go
@@ -42,10 +42,10 @@ type Info struct {
 	Username  string  `json:"user_name"`
 	Timestamp float64 `json:"timestamp"`
 
-	Pixel
+	Tile
 }
 
-type Pixel struct {
+type Tile struct {
 	Y     int          `json:"y"`
 	X     int          `json:"x"`
 	Color paletteIndex `json:"color"`
@@ -75,7 +75,6 @@ func GetBitmap() (image.Image, error) {
 
 	var whatIsThis [4]byte
 	r.Read(whatIsThis[:])
-	fmt.Println(whatIsThis)
 
 	for i := 0; i < len(img.Pix); i += 2 {
 		pix, err := r.ReadByte()
@@ -94,51 +93,20 @@ func GetBitmap() (image.Image, error) {
 }
 
 var DefaultColorPalette = color.Palette{
-	// ["#FFFFFF",
 	color.RGBA{0xFF, 0xFF, 0xFF, 0xFF},
-
-	//"#E4E4E4",
 	color.RGBA{0xE4, 0xE4, 0xE4, 0xFF},
-
-	//"#888888",
 	color.RGBA{0x88, 0x88, 0x88, 0xFF},
-
-	// "#222222",
 	color.RGBA{0x22, 0x22, 0x22, 0xFF},
-
-	// "#FFA7D1",
 	color.RGBA{0xFF, 0xA7, 0xD1, 0xFF},
-
-	// "#E50000",
 	color.RGBA{0xE5, 0x00, 0x00, 0xFF},
-
-	// "#E59500",
 	color.RGBA{0xE5, 0x95, 0x00, 0xFF},
-
-	// "#A06A42",
 	color.RGBA{0xA0, 0x6A, 0x42, 0xFF},
-
-	// "#E5D900",
 	color.RGBA{0xE5, 0xD9, 0x00, 0xFF},
-
-	// "#94E044",
 	color.RGBA{0x94, 0xE0, 0x44, 0xFF},
-
-	// "#02BE01",
 	color.RGBA{0x02, 0xBE, 0x01, 0xFF},
-
-	// "#00D3DD",
 	color.RGBA{0x00, 0xD3, 0xDD, 0xFF},
-
-	// "#0083C7",
 	color.RGBA{0x00, 0x83, 0xC7, 0xFF},
-
-	// "#0000EA",
 	color.RGBA{0x00, 0x00, 0xEA, 0xFF},
-
-	// "#CF6EE4",
 	color.RGBA{0xCF, 0x6E, 0xE4, 0xFF},
-
-	// "#820080"]
 	color.RGBA{0x82, 0x00, 0x80, 0xFF},
 }
